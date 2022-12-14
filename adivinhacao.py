c8fd1e7a2c658264dfd9b519cea0b18fd8faa3ba
ALXAVIER-DEV <alexandrexavier@outlook.com>
2022-12-14 20:58:27 -0300

    Modelo final

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,10 +1,25 @@
+import random
+
 print("**********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("**********************************")
 
-numero_secreto = 42
-total_tentativas = 3
-rodada = 1
+
+numero_secreto = random.randrange(1,101)
+total_tentativas = 0
+pontos = 1000
+
+print("Qual o nível de dificuldade ?")
+print("(1 Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Selecione seu nível: "))
+if(nivel == 1):
+    total_tentativas = 20
+elif(nivel == 2):
+    total_tentativas = 10
+else:
+    total_tentativas = 5
+
 for rodada in range(1, total_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_tentativas))
     chute = int(input("Digite um número entre 1 e 100: "))
@@ -18,13 +33,15 @@ for rodada in range(1, total_tentativas + 1):
     menor = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou Miseravil !!!")
+        print("Você acertou Miseravil e fez {} pontos!!!!".format(pontos))
         break
     else:
         if(maior):
             print("Errrrrrooooouuu o seu chute foi maior que o número secreto.")
         elif(menor):
             print("Errrrrrooooouuu o seu chute foi menor que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
 
 
